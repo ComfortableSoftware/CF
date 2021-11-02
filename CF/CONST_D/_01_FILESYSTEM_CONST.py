09f3631d90f1990b0a05a32620aa2fad73ca4f82
GaelicGrime <will.angus.blaylock@gmail.com>
2021-11-01 20:48:58 -0600

    Updated _01_HASHES.py.

--- CF/CONST_D/_01_FILESYSTEM_CONST.py
+++ CF/CONST_D/_01_FILESYSTEM_CONST.py
@@ -15,23 +15,24 @@ import re as RE
 import shutil as SHU
 
 
-from CF.KEYS_D import _01_FILESYSTEM_KEYS as FSK
+from CF.KEYS_D import _01_FILESYSTEM_KEYS as CF_FSK
+from CF.SUBM_D import _00_OS_P as CF_OSP
 
 
-CHMOD = os.chmod
-MKDIR = os.mkdir
-MOVE = shutil.move
-OS_ACCESS = os.access
-RENAME = os.rename
-RM = os.remove
-RMDIR = os.rmdir
-STAT = os.stat
-SUB = re.sub
+CHMOD = OS.chmod
+MKDIR = OS.mkdir
+MOVE = SHU.move
+OS_ACCESS = OS.access
+RENAME = OS.rename
+RM = OS.remove
+RMDIR = OS.rmdir
+STAT = OS.stat
+SUB = RE.sub
 
 
 CHMODDIR = 0o755  # default mode for directories this program needs to oerate in
 CHMODFILE = 0o655  # default mode for files this program needs to oerate in
-CONFIGDIR = f"""{HOME}/.config/{CFFS_V.__NAME__}"""  # default mode for files this program needs to oerate in
+# CONFIGDIR = f"""{CF_OSP.HOME}/.config/{CFFS_V.__NAME__}"""  # default mode for files this program needs to oerate in
 DIRBLACKLIST = "[a-zA-Z0-9./]+"  # PCRE for directory characters to blacklist as it's result
 DIRWHITELIST = "[^a-zA-Z0-9./]+"  # PCRE for directory characters to whitelist as it's result
 FILEBLACKLIST = "[a-zA-Z0-9.]+"  #
@@ -93,33 +94,33 @@ ILLEGALWILDCARDS = [  # list all of the portions of a filename which will result
 # * #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
 
 EMPTYENTRYTUP = (
-	(FSK.BLACKTAIL, ""),  # holds the blacklisted filename tail
-	(FSK.EXTENSIONGUESSED, ""),  # whole extension
-	(FSK.EXTHEAD, ""),  # whole FILENAME
-	(FSK.EXTTAIL, ""),  # whole FILENAME
-	(FSK.FILETYPEEXT, UNKNOWN),  # filetype group flag
-	(FSK.FILETYPEFILE, UNKNOWN),  # filetype group flag
-	(FSK.FILETYPEID, UNKNOWN),  # filetype group flag
-	(FSK.GID, ""),  # path tail
-	(FSK.ICANEXECUTE, ""),  # path tail
-	(FSK.ICANREAD, ""),  # path tail
-	(FSK.ICANWRITE, ""),  # path tail
-	(FSK.ISADIR, False),  # entry is a directory
-	(FSK.ISAFILE, False),  # entry is a file
-	(FSK.ISAKNOWNFILETYPE, False),  # in an unknown filetype
-	(FSK.ISASYMLINK, False),  # is a synlink
-	(FSK.MODE, 0),  # mode bits
-	(FSK.PATH, ""),  # whole path
-	(FSK.PATHHEAD, ""),  # path head
-	(FSK.PATHTAIL, ""),  # path tail
-	(FSK.RAWENTRY, ""),  # path tail
-	(FSK.SIZE, 0),  # size of file, 0/1 for a directory if empty/not-empty
-	(FSK.UID, ""),  # path tail
-	(FSK.WHITEEXTHEAD, ""),  #
-	(FSK.WHITEEXTTAIL, ""),  #
-	(FSK.WHITEFILENAME, ""),  # white filename
-	(FSK.WHITEFULLNAME, ""),  # white path+filename
-	(FSK.WHITEFULLPATH, ""),  # white full path
+	(CF_FSK.BLACKTAIL, ""),  # holds the blacklisted filename tail
+	(CF_FSK.EXTENSIONGUESSED, ""),  # whole extension
+	(CF_FSK.EXTHEAD, ""),  # whole FILENAME
+	(CF_FSK.EXTTAIL, ""),  # whole FILENAME
+	(CF_FSK.FILETYPEEXT, CF_FSK.UNKNOWN),  # filetype group flag
+	(CF_FSK.FILETYPEFILE, CF_FSK.UNKNOWN),  # filetype group flag
+	(CF_FSK.FILETYPEID, CF_FSK.UNKNOWN),  # filetype group flag
+	(CF_FSK.GID, ""),  # path tail
+	(CF_FSK.ICANEXECUTE, ""),  # path tail
+	(CF_FSK.ICANREAD, ""),  # path tail
+	(CF_FSK.ICANWRITE, ""),  # path tail
+	(CF_FSK.ISADIR, False),  # entry is a directory
+	(CF_FSK.ISAFILE, False),  # entry is a file
+	(CF_FSK.ISAKNOWNFILETYPE, False),  # in an unknown filetype
+	(CF_FSK.ISASYMLINK, False),  # is a synlink
+	(CF_FSK.MODE, 0),  # mode bits
+	(CF_FSK.PATH, ""),  # whole path
+	(CF_FSK.PATHHEAD, ""),  # path head
+	(CF_FSK.PATHTAIL, ""),  # path tail
+	(CF_FSK.RAWENTRY, ""),  # path tail
+	(CF_FSK.SIZE, 0),  # size of file, 0/1 for a directory if empty/not-empty
+	(CF_FSK.UID, ""),  # path tail
+	(CF_FSK.WHITEEXTHEAD, ""),  #
+	(CF_FSK.WHITEEXTTAIL, ""),  #
+	(CF_FSK.WHITEFILENAME, ""),  # white filename
+	(CF_FSK.WHITEFULLNAME, ""),  # white path+filename
+	(CF_FSK.WHITEFULLPATH, ""),  # white full path
 )
 
 def EMPTYENTRYDICT():
@@ -134,7 +135,7 @@ def EMPTYENTRYDICT():
 EXTENSIONSTYPES = {
 	# fold here ⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1
 
-	FSK.CODE:  [# filetype code
+	CF_FSK.CODE:  [# filetype code
 	# fold here ⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2
 		".c",  # c code file
 		".C",  # c code file
@@ -145,22 +146,22 @@ EXTENSIONSTYPES = {
 	],
 	# fold here ⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2
 
-	FSK.DOCS:  [# filetype docs
+	CF_FSK.DOCS:  [# filetype docs
 	# fold here ⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2
 	],
 	# fold here ⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2
 
-	FSK.KNOWN:  [# filetype known
+	CF_FSK.KNOWN:  [# filetype known
 	# fold here ⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2
 	],
 	# fold here ⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2
 
-	FSK.MEDIA:  [# filetype media
+	CF_FSK.MEDIA:  [# filetype media
 	# fold here ⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2
 	],
 	# fold here ⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2
 
-	FSK.PICS:  [# filetype pics
+	CF_FSK.PICS:  [# filetype pics
 	# fold here ⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2
 		".bmp",  # bmp image file
 		".BMP",  # bmp image file
@@ -177,7 +178,7 @@ EXTENSIONSTYPES = {
 	],
 	# fold here ⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2
 
-	FSK.TEXT:  [# filetype text
+	CF_FSK.TEXT:  [# filetype text
 	# fold here ⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2
 		".lst",  # text file
 		".LST",  # text file
@@ -186,12 +187,12 @@ EXTENSIONSTYPES = {
 	],
 	# fold here ⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2
 
-	FSK.UNKNOWN:  [# filetype unknown
+	CF_FSK.UNKNOWN:  [# filetype unknown
 	# fold here ⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2
 	],
 	# fold here ⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2⟰2
 
-	FSK.VIDS:  [# filetype video
+	CF_FSK.VIDS:  [# filetype video
 	# fold here ⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2⟱2
 		".asx",  # asx/wmv video file
 		".ASX",  # asx/wmv video file
@@ -239,125 +240,125 @@ EXTENSIONSTYPES = {
 
 EXTENSIONLOOKUP = {
 	# fold here ⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1
-	".asx": FSK.VIDS,  # asx/wmv video file
-	".ASX": FSK.VIDS,  # asx/wmv video file
-	".avi": FSK.VIDS,  # avi video file
-	".AVI": FSK.VIDS,  # avi video file
-	".bmp": FSK.PICS,  # bmp image file
-	".BMP": FSK.PICS,  # bmp image file
-	".c": FSK.CODE,  # c code file
-	".C": FSK.CODE,  # c code file
-	".divx": FSK.VIDS,  # divx video file
-	".DIVX": FSK.VIDS,  # divx video file
-	".flv": FSK.VIDS,  # flv video file
-	".FLV": FSK.VIDS,  # flv video file
-	".gif": FSK.PICS,  # gif image file
-	".GIF": FSK.PICS,  # gif image file
-	".gifv": FSK.VIDS,  # gifv video file
-	".GIFV": FSK.VIDS,  # gifv video file
-	".jpeg": FSK.PICS,  # jpeg image file
-	".JPEG": FSK.PICS,  # jpeg image file
-	".jpg": FSK.PICS,  # jpg image file
-	".JPG": FSK.PICS,  # jpg image file
-	".lst": FSK.TEXT,  # text file
-	".LST": FSK.TEXT,  # text file
-	".lst.txt": FSK.TEXT,  # text file
-	".LST.TXT": FSK.TEXT,  # text file
-	".m2ts": FSK.VIDS,  # m2ts video file
-	".M2TS": FSK.VIDS,  # m2ts video file
-	".m4a": FSK.VIDS,  # m4a video file
-	".M4A": FSK.VIDS,  # m4a video file
-	".m4v": FSK.VIDS,  # m4v video file
-	".M4V": FSK.VIDS,  # m4v video file
-	".mkv": FSK.VIDS,  # mkv video file
-	".MKV": FSK.VIDS,  # mkv video file
-	".mov": FSK.VIDS,  # mov video file
-	".MOV": FSK.VIDS,  # mov video file
-	".mp4": FSK.VIDS,  # mp4 video file
-	".MP4": FSK.VIDS,  # mp4 video file
-	".mpeg": FSK.VIDS,  # mpeg video file
-	".MPEG": FSK.VIDS,  # mpeg video file
-	".mpg": FSK.VIDS,  # mpg video file
-	".MPG": FSK.VIDS,  # mpg video file
-	".php": FSK.CODE,  # php code file
-	".PHP": FSK.CODE,  # php code file
-	".png": FSK.PICS,  # png image file
-	".PNG": FSK.PICS,  # png image file
-	".py": FSK.CODE,  # python code file
-	".PY": FSK.CODE,  # python code file
-	".webm": FSK.VIDS,  # webm video file
-	".WEBM": FSK.VIDS,  # webm video file
-	".webp": FSK.PICS,  # webp image file
-	".WEBP": FSK.PICS,  # webp image file
-	".wmv": FSK.VIDS,  # wmv video file
-	".WMV": FSK.VIDS,  # wmv video file
+	".asx": CF_FSK.VIDS,  # asx/wmv video file
+	".ASX": CF_FSK.VIDS,  # asx/wmv video file
+	".avi": CF_FSK.VIDS,  # avi video file
+	".AVI": CF_FSK.VIDS,  # avi video file
+	".bmp": CF_FSK.PICS,  # bmp image file
+	".BMP": CF_FSK.PICS,  # bmp image file
+	".c": CF_FSK.CODE,  # c code file
+	".C": CF_FSK.CODE,  # c code file
+	".divx": CF_FSK.VIDS,  # divx video file
+	".DIVX": CF_FSK.VIDS,  # divx video file
+	".flv": CF_FSK.VIDS,  # flv video file
+	".FLV": CF_FSK.VIDS,  # flv video file
+	".gif": CF_FSK.PICS,  # gif image file
+	".GIF": CF_FSK.PICS,  # gif image file
+	".gifv": CF_FSK.VIDS,  # gifv video file
+	".GIFV": CF_FSK.VIDS,  # gifv video file
+	".jpeg": CF_FSK.PICS,  # jpeg image file
+	".JPEG": CF_FSK.PICS,  # jpeg image file
+	".jpg": CF_FSK.PICS,  # jpg image file
+	".JPG": CF_FSK.PICS,  # jpg image file
+	".lst": CF_FSK.TEXT,  # text file
+	".LST": CF_FSK.TEXT,  # text file
+	".lst.txt": CF_FSK.TEXT,  # text file
+	".LST.TXT": CF_FSK.TEXT,  # text file
+	".m2ts": CF_FSK.VIDS,  # m2ts video file
+	".M2TS": CF_FSK.VIDS,  # m2ts video file
+	".m4a": CF_FSK.VIDS,  # m4a video file
+	".M4A": CF_FSK.VIDS,  # m4a video file
+	".m4v": CF_FSK.VIDS,  # m4v video file
+	".M4V": CF_FSK.VIDS,  # m4v video file
+	".mkv": CF_FSK.VIDS,  # mkv video file
+	".MKV": CF_FSK.VIDS,  # mkv video file
+	".mov": CF_FSK.VIDS,  # mov video file
+	".MOV": CF_FSK.VIDS,  # mov video file
+	".mp4": CF_FSK.VIDS,  # mp4 video file
+	".MP4": CF_FSK.VIDS,  # mp4 video file
+	".mpeg": CF_FSK.VIDS,  # mpeg video file
+	".MPEG": CF_FSK.VIDS,  # mpeg video file
+	".mpg": CF_FSK.VIDS,  # mpg video file
+	".MPG": CF_FSK.VIDS,  # mpg video file
+	".php": CF_FSK.CODE,  # php code file
+	".PHP": CF_FSK.CODE,  # php code file
+	".png": CF_FSK.PICS,  # png image file
+	".PNG": CF_FSK.PICS,  # png image file
+	".py": CF_FSK.CODE,  # python code file
+	".PY": CF_FSK.CODE,  # python code file
+	".webm": CF_FSK.VIDS,  # webm video file
+	".WEBM": CF_FSK.VIDS,  # webm video file
+	".webp": CF_FSK.PICS,  # webp image file
+	".WEBP": CF_FSK.PICS,  # webp image file
+	".wmv": CF_FSK.VIDS,  # wmv video file
+	".WMV": CF_FSK.VIDS,  # wmv video file
 }
 # fold here ⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1
 
 
 EXTENSIONGROUPS = {
-	FSK.CODE: FSK.GTEXT,  # filetype code
-	FSK.DOCS: FSK.GNONE,  # filetype docs
-	FSK.KNOWN: FSK.GKNOWN,  # filetype known
-	FSK.MEDIA: FSK.GMEDIA,  # filetype media
-	FSK.PICS: FSK.GMEDIA,  # filetype pics
-	FSK.TEXT: FSK.GTEXT,  # filetype text
-	FSK.UNKNOWN: FSK.GNONE,  # filetype unknown
-	FSK.VIDS: FSK.GMEDIA,  # filetype video
+	CF_FSK.CODE: CF_FSK.GTEXT,  # filetype code
+	CF_FSK.DOCS: CF_FSK.GNONE,  # filetype docs
+	CF_FSK.KNOWN: CF_FSK.GKNOWN,  # filetype known
+	CF_FSK.MEDIA: CF_FSK.GMEDIA,  # filetype media
+	CF_FSK.PICS: CF_FSK.GMEDIA,  # filetype pics
+	CF_FSK.TEXT: CF_FSK.GTEXT,  # filetype text
+	CF_FSK.UNKNOWN: CF_FSK.GNONE,  # filetype unknown
+	CF_FSK.VIDS: CF_FSK.GMEDIA,  # filetype video
 }
 
 
 INDEXES = {
-	FSK.CODE: 0,  # filetype code
-	FSK.DOCS: 0,  # filetype docs
-	FSK.KNOWN: 0,  # filetype known
-	FSK.MEDIA: 0,  # filetype media
-	FSK.PICS: 0,  # filetype pics
-	FSK.TEXT: 0,  # filetype text
-	FSK.UNKNOWN: 0,  # filetype unknown
-	FSK.VIDS: 0,  # filetype video
+	CF_FSK.CODE: 0,  # filetype code
+	CF_FSK.DOCS: 0,  # filetype docs
+	CF_FSK.KNOWN: 0,  # filetype known
+	CF_FSK.MEDIA: 0,  # filetype media
+	CF_FSK.PICS: 0,  # filetype pics
+	CF_FSK.TEXT: 0,  # filetype text
+	CF_FSK.UNKNOWN: 0,  # filetype unknown
+	CF_FSK.VIDS: 0,  # filetype video
 }
 
 
 SPREADS = {
-	FSK.SPREADCODE: False,  # zfiletype code
-	FSK.SPREADDOCS: False,  # filetype docs
-	FSK.SPREADKNOWN: False,  # filetype known
-	FSK.SPREADMEDIA: True,  # filetype media
-	FSK.SPREADPICS: True,  # filetype pics
-	FSK.SPREADTEXT: False,  # filetype text
-	FSK.SPREADUNKNOWN: False,  # filetype unknown
-	FSK.SPREADVIDS: True,  # filetype video
+	CF_FSK.SPREADCODE: False,  # zfiletype code
+	CF_FSK.SPREADDOCS: False,  # filetype docs
+	CF_FSK.SPREADKNOWN: False,  # filetype known
+	CF_FSK.SPREADMEDIA: True,  # filetype media
+	CF_FSK.SPREADPICS: True,  # filetype pics
+	CF_FSK.SPREADTEXT: False,  # filetype text
+	CF_FSK.SPREADUNKNOWN: False,  # filetype unknown
+	CF_FSK.SPREADVIDS: True,  # filetype video
 }
 
 
 WHATTODO = {
-	FSK.DOCODE: False,  # zfiletype code
-	FSK.DODOCS: False,  # filetype docs
-	FSK.DOKNOWN: False,  # filetype known
-	FSK.DOMEDIA: True,  # filetype media
-	FSK.DOPICS: True,  # filetype pics
-	FSK.DOTEXT: False,  # filetype text
-	FSK.DOUNKNOWN: False,  # filetype unknown
-	FSK.DOVIDS: True,  # filetype video
+	CF_FSK.DOCODE: False,  # zfiletype code
+	CF_FSK.DODOCS: False,  # filetype docs
+	CF_FSK.DOKNOWN: False,  # filetype known
+	CF_FSK.DOMEDIA: True,  # filetype media
+	CF_FSK.DOPICS: True,  # filetype pics
+	CF_FSK.DOTEXT: False,  # filetype text
+	CF_FSK.DOUNKNOWN: False,  # filetype unknown
+	CF_FSK.DOVIDS: True,  # filetype video
 }
 
 
 EXTENSIONTYPETRANS = {
-	FSK.DOCODE: FSK.CODE,
-	FSK.DODOCS: FSK.DOCS,
-	FSK.DOKNOWN: FSK.KNOWN,
-	FSK.DOMEDIA: FSK.MEDIA,
-	FSK.DOPICS: FSK.PICS,
-	FSK.DOTEXT: FSK.TEXT,
-	FSK.DOUNKNOWN: FSK.UNKNOWN,
-	FSK.DOVIDS: FSK.VIDS,
-	FSK.SPREADCODE: FSK.CODE,
-	FSK.SPREADDOCS: FSK.DOCS,
-	FSK.SPREADKNOWN: FSK.KNOWN,
-	FSK.SPREADMEDIA: FSK.MEDIA,
-	FSK.SPREADPICS: FSK.PICS,
-	FSK.SPREADTEXT: FSK.TEXT,
-	FSK.SPREADUNKNOWN: FSK.UNKNOWN,
-	FSK.SPREADVIDS: FSK.VIDS,
+	CF_FSK.DOCODE: CF_FSK.CODE,
+	CF_FSK.DODOCS: CF_FSK.DOCS,
+	CF_FSK.DOKNOWN: CF_FSK.KNOWN,
+	CF_FSK.DOMEDIA: CF_FSK.MEDIA,
+	CF_FSK.DOPICS: CF_FSK.PICS,
+	CF_FSK.DOTEXT: CF_FSK.TEXT,
+	CF_FSK.DOUNKNOWN: CF_FSK.UNKNOWN,
+	CF_FSK.DOVIDS: CF_FSK.VIDS,
+	CF_FSK.SPREADCODE: CF_FSK.CODE,
+	CF_FSK.SPREADDOCS: CF_FSK.DOCS,
+	CF_FSK.SPREADKNOWN: CF_FSK.KNOWN,
+	CF_FSK.SPREADMEDIA: CF_FSK.MEDIA,
+	CF_FSK.SPREADPICS: CF_FSK.PICS,
+	CF_FSK.SPREADTEXT: CF_FSK.TEXT,
+	CF_FSK.SPREADUNKNOWN: CF_FSK.UNKNOWN,
+	CF_FSK.SPREADVIDS: CF_FSK.VIDS,
 }
