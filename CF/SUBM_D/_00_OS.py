f604b8340b2abe3ea8870d2409530bd6a71819d0
GaelicGrime <will.angus.blaylock@gmail.com>
2022-04-02 00:55:49 -0600

    Updated _00_STRING_STUFF.py.

--- CF/SUBM_D/_00_OS.py
+++ CF/SUBM_D/_00_OS.py
@@ -15,18 +15,35 @@ from os import path as OS_PATH
 
 ABS_PATH = OS_PATH.abspath
 BASE_NAME = OS_PATH.basename
+COMMON_PATH = OS_PATH.commonpath
+COMMON_PREFIX = OS_PATH.commonprefix
 DIR_NAME = OS_PATH.dirname
 EXISTS = OS_PATH.exists
+EXPAND_USER = OS_PATH.expanduser
+EXPAND_VARS = OS_PATH.expandvars
 GET_A_TIME = OS_PATH.getatime
 GET_C_TIME = OS_PATH.getctime
 GET_M_TIME = OS_PATH.getmtime
 GET_SIZE = OS_PATH.getsize
 HOME = f"""{OS_PATH.expanduser('~')}"""
+IS_ABS = OS_PATH.isabs
 IS_DIR = OS_PATH.isdir
 IS_FILE = OS_PATH.isfile
 IS_LINK = OS_PATH.islink
 IS_MOUNT = OS_PATH.ismount
+JOIN = OS_PATH.join
+L_EXISTS = OS_PATH.lexists
+NORM_CASE = OS_PATH.normcase
 NORM_PATH = OS_PATH.normpath
+REAL_PATH = OS_PATH.realpath
+REL_PATH = OS_PATH.relpath
+SAME_FILE = OS_PATH.samefile
+SAME_OPEN_FILE = OS_PATH.sameopenfile
+SAME_STAT = OS_PATH.samestat
+SPLIT = OS_PATH.split
+SPLIT_DRIVE = OS_PATH.splitdrive
+SPLIT_EXT = OS_PATH.splitext
+SUPPORTS_UNICODE_FILENAMES = OS_PATH.supports_unicode_filenames
 
 
 ABS_DOT = ABS_PATH(".")
