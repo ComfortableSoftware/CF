315539cf034b74b461c0c9098d5d2b27b122c280
GaelicGrime <will.angus.blaylock@gmail.com>
2022-05-11 21:16:07 -0600

    Updated _01_OPTIONS_VARS.py.

---
+++ CF/SUBM_D/_01_OPTIONS_VARS.py
@@ -0,0 +1,56 @@
+
+
+from sys import argv
+import argparse as AP
+from CF.CONST_D import _00_VALS as CF_V
+
+
+V = None
+ARGV = argv
+ARGS = None
+PARSER = None
+
+
+
+
+ARG_PARSER_TUP = (
+    ("prog", None),
+    ("usage", None),
+    ("description", None),
+    ("epilog", None),
+    ("parents", []),
+    ("formatter_class", AP.HelpFormatter),
+    ("prefix_chars", "-"),
+    ("fromfile_prefix_chars", None),
+    ("argument_default", None),
+    ("conflict_handler", "error"),
+    ("add_help", True),
+    ("allow_abbrev", True),
+    ("exit_on_error", True),
+)
+def E_ARG_PARSER():
+  return dict((x, y) for x, y in ARG_PARSER_TUP)
+
+
+ADD_ARGS_TUP = (
+    ("option_strings", ""),  # "-b" or ["-b", "-bigOne"] dest= must be set if this is used
+    ("dest", None),
+    ("nargs", None),
+    ("const", None),
+    ("default", None),
+    ("type", None),
+    ("choices", None),
+    ("required", False),
+    ("help", None),
+    ("metavar", None),
+)
+def E_ADD_ARGS():
+  return dict((x, y) for x, y in ADD_ARGS_TUP)
+
+
+
+
+
+
+
+#
