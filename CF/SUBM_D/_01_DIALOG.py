478570618b4f21f8901197c4bfffa366822612c2
GaelicGrime <will.angus.blaylock@gmail.com>
2022-11-01 22:33:32 -0600

    Ready to start filling kdialog code in

--- CF/SUBM_D/_01_DIALOG.py
+++ CF/SUBM_D/_01_DIALOG.py
@@ -15,7 +15,9 @@ def checkProgressBar(*,
   pass
 
 
-def closeProgressbar():
+def closeProgressbar(*,
+    dialogPath_
+):
   # Returns _result_
   pass
 
@@ -45,6 +47,99 @@ def doA3BtnDialog(*,
   # ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1
 
 
+def doACalendar (*,
+    default_,  # Defaults to today
+    format_="ddd MMM d yyyy",  # QT style
+    text_,
+    title_="Select a date",
+):
+  pass
+
+
+def doAChecklist (*,
+    checkList_,  # [(index (hidden), text, on/off/"" (set state)/hidden) …] returns with stdout == index index … of any items that were clicked on or defaulted on and not offed
+    text_,
+    title_="Click all that apply:",
+):
+  pass
+
+
+def doACombobox (*
+    default_,  #
+    itemList_,  # ["item 1", "item 2", …] returns
+    text_,
+    title_="Select one:",
+):
+  pass
+
+
+def doADetailedErrorDialog(*,
+    details_,
+    text_,
+    title_="Something went wrong",
+    where_,
+):
+  pass
+
+
+def doADetailedWarningDialog(*,
+    details_,
+    text_=,
+    title_="Something is fishy",
+    where_,
+):
+  pass
+
+
+def doAMenu (*,
+    default_,  # No default unless you enter it
+    items_,  # [("index (hidden)" "description)") …]
+    text_,
+    title_="Select your choice.",
+):
+  pass
+
+
+def doAMessageBox(*,
+    okLabel_="OK",
+    text_,
+    title_="Message for you",
+):
+  pass
+
+
+def doAnErrorDialog(*,
+    text_,
+    title_="Something went wrong",
+    where_,
+):
+  pass
+
+
+def doAnInputbox(*,
+    default_="",
+    text_,
+    title_="Input a line",
+):
+  pass
+
+
+def doAPassivepopup (*,
+    text_,
+    timeout_="10",
+    title_="An annoying popup",
+):
+  pass
+
+
+def doARadiolist (*,
+    items_,  # [(index description on/off/"")]  Last one listed as "on" will be the default selection
+    text_,
+    title_="Pick one",
+):
+  pass
+
+
 def doATextbox(*,
     filename_,
     height_=300,
@@ -54,8 +149,64 @@ def doATextbox(*,
   pass
 
 
+def doATextInputBox(*,
+    default_="",
+    filename_,
+    height_=300,
+    text_,
+    title_="Heard you liked some reading/typing yo‼",
+    width_=700,
+):
+  pass
+
+
+def doAWarningDialog(*,
+    text_,
+    title_="Something is fishy",
+    where_,
+):
+  pass
+
+
+def doGetExistingDirectory (*
+    title_="Select a directory",
+    startDir_=".",
+):
+  pass
+
+
+def doGetOpenFilename (*,
+    title_="Pick a file",
+    startDir_,
+    filter_="",  # "mime/type mime/type …" or "name (*.ext *.ext …)" or "name (filename.ext …)"
+):
+  pass
+
+
+def doGetSaveFilename (*,
+    title_="Pick a file",
+    startDir_,
+    filter_="",  # "mime/type mime/type …" or "name (*.ext *.ext …)" or "name (filename.ext …)"
+):
+  pass
+
+
+def enterANewPassword (*,
+    text_,
+    title_="Enter a new password",
+):
+  pass
+
+
+def inputAPassword (*,
+    text_,
+    title_="Your password, enter it",
+):
+  pass
+
+
 def setProgressVals(*,
-    autoClose_=None,
+    autoClose_=True,
     dialogPath_,
     maximum_=None,
     showCancelButton_=None,
@@ -67,15 +218,15 @@ def setProgressVals(*,
 
 
 def startAProgressBar(*,
-    autoClose=False,  # Auto closes when 100% is reached.
+    autoClose=True,  # Auto closes when 100% is reached.
     numOfSteps_==100,
     showCancelButton_=True,
     text_="Progress:",
     title_="Getting things done",
     value=0,
 ):
+  # 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱ 0⟱
   ## Returns _result_
-  # 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱
   _commands_ = [
       "kdialog",
       "--title",
@@ -85,13 +236,58 @@ def startAProgressBar(*,
       f"""{numOfSteps_}""",
   ]
   _result1_ V.runIt(_commands_)
+  _result1_ = V.parseResult(
+      _result1_,
+      startingProgressbar=True,
+  )
   # 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱
   if (
-      (showCancelButton_ is not True)
+      (_result1_[K_RETURN_CODE] != 0)
   ):
+    V.runError("starting a progress bar", _result1_)
+  # ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1
+  _commands_ = [
+      "qdbus",
+      f"""{_result1_[K_SERVICE_NAME]}""",
+      f"""{_result1_[K_SERVICE_PATH]}""",
+      V.SM_PROGRESSBAR_SHOW_CANCEL_BUTTON,
+      f"""{showCancelButton_}""".lower(),
+  ]
+  _result2_ = V.runIt(_commands_)
+  _result2_ = V.parseResult(_result2_)
+  # 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱
+  if (
+      (_result2_[K_RETURN_CODE] != 0)
+  ):
+    V.runError("setting the cancel button on a progress bar", _result2_)
+  # ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1
+  _commands_ = [
+      "qdbus",
+      f"""{_result1_[K_SERVICE_NAME]}""",
+      f"""{_result1_[K_SERVICE_PATH]}""",
+      V.SM_PROGRESSBAR_AUTOCLOSE,
+      f"""{autoClose_}""".lower(),
+  ]
+  _result3_ = V.runIt(_commands_)
+  _result3_ = V.parseResult(_result2_)
+  # 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱
+  if (
+      (_result3_[K_RETURN_CODE] != 0)
+  ):
+    V.runError("setting the auto close on a progress bar", _result3_)
+  # ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1
+  return _result1_
+  # ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0 ⟰0
 
-  # ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1
-  # ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1
+
+def updateAProgressbar (*,
+    dialogPath_,
+    maximum_=None,
+    value=None,
+    autoClose_=None,
+    showCancelButton_=None,
+):
+  pass
 
 
 
--- CF/SUBM_D/_01_DIALOG_VARS.py
+++ CF/SUBM_D/_01_DIALOG_VARS.py
@@ -10,22 +10,80 @@ V = None
 locals().update(CF_V.ALL_THE_VALS)
 
 
+SM_PROGRESSBAR_AUTOCLOSE = "org.kde.kdialog.ProgressDialog.autoclose"
+SM_PROGRESSBAR_MAXIMUM = "org.kde.kdialog.ProgressDialog.maximum"
+SM_PROGRESSBAR_SHOW_CANCEL_BUTTON = "org.kde.kdialog.ProgressDialog.showCancelButton"
+SM_PROGRESSBAR_VALUE = "org.kde.kdialog.ProgressDialog.value"
+SM_PROGRESSBAR_WASCANCELLED = "org.kde.kdialog.ProgressDialog.wasCancelled"
+
+
 ALL_THE_KEYS = {
+  "K_ARGS": "K_ARGS",
+  "K_RETURN_CODE": "K_RETURN_CODE",
+  "K_RESULT": "K_RESULT",
   "K_SERVICE_ARGS": "K_SERVICE_ARGS",
   "K_SERVICE_METHOD": "K_SERVICE_METHOD",
   "K_SERVICE_NAME": "K_SERVICE_NAME",
   "K_SERVICE_PATH": "K_SERVICE_PATH",
+  "K_STDERR": "K_STDERR",
+  "K_STDIN": "K_STDIN",
+  "K_STDOUT": "K_STDOUT",
 }
 locals().update(ALL_THE_KEYS)
 
 
-def parseResult(resultToParse_):
+def parseResult(
+    resultToParse_,
+    *,
+    startingProgressbar=False,
+):
   # 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱
-  pass
+  _stdOut_ = resultToParse_.stdout
+  # 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱
+  if (
+      (resultToParse_.returncode == 0) and
+      (startingProgressbar is True)
+  ):
+    _splitStdout_ = _stdOut_.split(" ")
+  else:
+    _splitStdout_ = ["", ""]
+  # ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1
+  return {
+      K_ARGS: resultToParse_.args,
+      # K_RESULT: resultToParse_,
+      K_RETURN_CODE: resultToParse_.returncode,
+      K_SERVICE_NAME: _splitStdout_[0],
+      K_SERVICE_PATH: _splitStdout_[1],
+      K_STDERR: resultToParse_.stderr,
+      K_STDIN: resultToParse_.stdin,
+      K_STDOUT: resultToParse_.stdout,
+  }
   # ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1
 
 
+def runError(errorText_, errorResult_):
+  _text_ = f"""An error occurred while {errorText_}"""
+  _detail_ = f"""Args: `{errorResult_[K_ARGS]}`
+Return code: {errorResult_[K_RETURN_CODE]}
+Stderr: `{errorResult_[K_STDERR]}`
+Stdin: `{errorResult_[K_STDIN]}`
+Stdout: `{errorResult_[K_STDOUT]}`
+"""
+  _commands_ = [
+      "kdialog",
+      "--title",
+      "Kdialog Error",
+      "--detailederror",
+      _text_,
+      _detail_,
+  ]
+  V.runIt(_commands_)
+  Exit(errorResult_[K_RETURN_CODE]
+
+
+
 def runIt(commandsToRun_, *, **KWArgs_={}):
+  # 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱ 1⟱
   if (
       ("capture_output" not in KWArgs_)
   ):
@@ -36,8 +94,9 @@ def runIt(commandsToRun_, *, **KWArgs_={}):
   ):
     KWArgs_["text"] = True
 
-  SP.run(commandsToRun_, **KWArgs_)
-
+  _result_ = SP.run(commandsToRun_, **KWArgs_)
+  return _result1_
+  # ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1 ⟰1
 
 
 
