53ba1b0bf4e47d19e388e53b0dc1deb40956865e
GaelicGrime <will.angus.blaylock@gmail.com>
2023-03-12 21:48:17 -0600

    update for release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
   license="GPLv3",
   name="CS-CF",
   url="https://github.com/ComfortableSoftware/commonFunctions_py",
-  version="0.10.0",
+  version="0.10.0-1",
   package_dir={"CF": "CF"},
   package_data={
       "CF": [
@@ -32,7 +32,7 @@ setup(
           "SUBM_D/*",
       ]
   },
-  packages=["CF"],
+  packages=find_packages(),
   install_requires=[
   ],
   extras_require={
